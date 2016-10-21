486cfa8d503651819114b0cff2eaca8f8dbac654
ve156354 <veerendra_akula@yahoo.com>
2016-10-21 10:29:25 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
  <?php 
- echo "Hello openshift from Tech Dev Presentation...V 1.0 <br><br>";
+ echo "Hello openshift from Tech Dev Presentation...V 2.0 <br><br>";
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
